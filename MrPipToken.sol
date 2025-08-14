a92afecb62a86c24fa1355326afb62e1620a5216
MrPipToken <team@mrpip.club>
2025-08-14 11:08:53 +0300

    MrPipToken.sol

--- MrPipToken.sol
+++ MrPipToken.sol
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
+// Compatible with OpenZeppelin Contracts ^5.4.0
 pragma solidity ^0.8.27;
 
-import {ERC20} from "@openzeppelin/contracts@5.3.0/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "@openzeppelin/contracts@5.3.0/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20} from "@openzeppelin/contracts@5.4.0/token/ERC20/ERC20.sol";
 
-contract MrPIP is ERC20, ERC20Permit {
-    constructor(address recipient)
-        ERC20("Mr.PIP", "Mr.PIP")
-        ERC20Permit("Mr.PIP")
-    {
+contract MrPIP is ERC20 {
+    constructor(address recipient) ERC20("Mr.PIP", "MrPIP") {
         _mint(recipient, 10000000 * 10 ** decimals());
     }
 }
